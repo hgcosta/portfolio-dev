23065b0cd61938ee396d57839918a4595fc3a504
Hugo <hugocosta.devs@gmail.com>
2025-06-30 19:28:52 -0300

    feat: ajuste app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-// import Contact from "./components/Contact";
+import Contact from "./components/Contact";
 // import Footer from "./components/Footer";
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
       <About />
       <Projects />
       <Skills />
-      {/* <Contact />
-      <Footer /> */}
+      <Contact />
+      {/* <Footer /> */}
     </div>
   );
 }
